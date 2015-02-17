78c27bab456a17d968b90c2261a1fc4475fbf883
Lisa Wolfson <lwolfson@mcad.edu>
2015-02-16 19:59:11 -0600

    Added clearfix class per video (it didn't work for him, but seems to work for me). Included excerpt functions, even though they aren't currently in use.

--- functions.php
+++ functions.php
@@ -140,3 +140,19 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+/**
+* Change the length of the post excerpts
+*/
+function custom_excerpt_length( $length ) {
+	return 30;
+}
+add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
+
+/**
+* Change the ellipse to "Read More" at the end of the post excerpts
+*/
+function new_excerpt_more( $more ) {
+	return ' <a class="read-more" href="'. get_permalink( get_the_ID() ) . '">' . __('Read More', 'your-text-domain') . '</a>';
+}
+add_filter( 'excerpt_more', 'new_excerpt_more' );
