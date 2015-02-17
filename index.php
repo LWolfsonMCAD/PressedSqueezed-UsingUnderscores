e4734d0860e4d82fa199cccb29061fbbc624d414
Lisa Wolfson <lwolfson@mcad.edu>
2015-02-16 20:01:21 -0600

    Added homepage/index featured image vs WP admin "featured image" on post. Need to figure out bloginfo() for an image. Long-term it's probably better to go with post featured image for a blog, but for a homepage a featured image would work better(?). Need to figure out way to upload any image into that "spot" so user could easily change it.

--- index.php
+++ index.php
@@ -16,20 +16,23 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
+			<img src="http://localhost:8888/wp-content/themes/pressed/images/limes600x256.jpg" alt="Limes" id="homepage-featured-image">	
+
+			<?php if ( have_posts() ) : ?>
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php
+				<?php  
 				if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
 					the_post_thumbnail();
 				} 
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
+					
+				/* Include the Post-Format-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */ 
+				get_template_part( 'content', get_post_format() );
 
 				?>
 
