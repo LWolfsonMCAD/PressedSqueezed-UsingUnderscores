3989ec9a0c4066710e475fbc787f9998077c9da4
Lisa Wolfson <lwolfson@mcad.edu>
2015-02-16 19:59:53 -0600

    Messed around with putting image in header vs "featured image" through WP admin…ultimately decided against it.

--- header.php
+++ header.php
@@ -18,13 +18,15 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
+<div id="page" class="hfeed site clearfix">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'pressedsqueezed' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description invisible"><?php bloginfo( 'description' ); ?></h2>
+		
+		<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1> 
+
+		<h2 class="site-description invisible"><?php bloginfo( 'description' ); ?></h2>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
